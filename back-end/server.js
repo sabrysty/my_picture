bc469f6c6c8eea576486b7e7af717a282e794ffc
sabry-solal <sabry.zammit@solal.co>
2024-07-08 15:53:53 +0200

    add .env config

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const mysql = require('mysql')
+const dotenv = require('dotenv')
 
 
+const { parsed: config } = dotenv.config();
 const app = express()
-const optionBd = {
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    port: 3306,
-    database: 'picture'
+const dbOptions = {
+    host : config.DB_HOST,
+    user : config.DB_USER,
+    password : config.DB_PASSWORD,
+    port: config.DB_PORT,
+    database: config.DB_DATABASE,
 }
 
 
 app.listen(3001, "localhost", () => {
-    console.log("hello 3001")
+    console.log(dbOptions.database)
 })
